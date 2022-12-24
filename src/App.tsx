4f328049cd24d02de33438c1d1543cba46202d8b
Pierre Dias <cdias.pierre@gmail.com>
2022-12-24 14:46:19 -0300

    Ações do sistema

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import styles from "./App.module.css";
 import poweredImage from "./assets/powered.png";
-import { levels, calculateImc } from "./helpers/imc";
+import { levels, calculateImc, Level } from "./helpers/imc";
 import { GridItem } from "./components/GridItem";
 
 const App = () => {
   const [heightField, setHeightField] = useState<number>(0);
   const [weightField, setWeightField] = useState<number>(0);
+  const [toShow, setToShow] = useState<Level | null>(null);
 
   const handleCalculateButton = () => {
     if (heightField && weightField) {
+      setToShow(calculateImc(heightField, weightField));
     } else {
       alert("Digite todos os campos");
     }
@@ -49,11 +51,20 @@ const App = () => {
           <button onClick={handleCalculateButton}>Calcular</button>
         </div>
         <div className={styles.rightSide}>
-          <div className={styles.grid}>
-            {levels.map((item, index) => (
-              <GridItem key={index} item={item}></GridItem>
-            ))}
-          </div>
+          {!toShow && (
+            <div className={styles.grid}>
+              {levels.map((item, index) => (
+                <GridItem key={index} item={item}></GridItem>
+              ))}
+            </div>
+          )}
+
+          {toShow && (
+            <div className={styles.rightBig}>
+              <div className={styles.rightArrow}></div>
+              <GridItem item={toShow} />
+            </div>
+          )}
         </div>
       </div>
     </div>
